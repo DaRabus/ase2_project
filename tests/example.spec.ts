b4ece050c1d6982ff486377bb70cee225588ff0f
DaRabus <dominik.rabus@steiner.ch>
2024-05-01 16:16:59 +0200

    add more tests as example

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,65 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const NameToCategory: {
+  [key: number]: string;
+} = {
+  103: 'Bars and lounges in the Zurich region',
+  101: 'Eating out in Zurich',
+  96: 'Culture in the Zurich region',
+  136: 'Museums in the Zurich region',
+  162: 'Nightlife: clubs in Zurich'
+};
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.waitForTimeout(5000);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Check if all 5 categories are loaded', async ({ page }) => {
+  for (const key of Object.keys(NameToCategory)) {
+    await expect(page.getByTestId(`expand-button-${key}`)).toBeVisible();
+  }
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  for (const value of Object.values(NameToCategory)) {
+    await expect(page.locator(`text=${value}`)).toBeVisible();
+  }
+});
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Check if categories is opening and if it shows the amount of elements configured also on page switch', async ({
+  page
+}) => {
+  const firstElement = Object.keys(NameToCategory)[0];
+
+  console.log('firstElement', firstElement);
+
+  await page.getByTestId(`expand-button-${firstElement}`).click();
+
+  await page
+    .getByLabel(`store-element`)
+    .count()
+    .then((count) => {
+      expect(count).toBe(3);
+    });
+
+  await page.getByTestId(`pagination-${firstElement}`).getByText('2').click();
+
+  await page
+    .getByLabel(`store-element`)
+    .count()
+    .then((count) => {
+      expect(count).toBe(3);
+    });
+
+  // Switch page size selector
+
+  await page.getByTestId(`page-size-select`).click();
+  await page.getByTestId(`page-size-5`).click();
+
+  await page
+    .getByLabel(`store-element`)
+    .count()
+    .then((count) => {
+      expect(count).toBe(5);
+    });
 });
